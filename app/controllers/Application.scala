156bb7f8fd6800a3d4f1e6fea1b57f8111b7479c
zhengda <leonlin0403@gmail.com>
2016-10-16 00:04:47 +0800

    extra fields for fb and lineid

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -58,16 +58,40 @@ class Application @Inject()(db: Database, dbapi: DBApi) extends Controller {
     }
   }
 
-  def repack(x: Liwei): Liwei = {
+  def repack(x: Liwei) = {
     import java.net.URLDecoder
     val email = if (x.email contains "不提供") {
       ""
     } else {
       x.email
     }
-    val facebook = (x.facebook + "\n").replace("?fref=ts\n", "")
+    var facebook2 = ""
+    var facebook = x.facebook
+    facebook = URLDecoder.decode(facebook, "utf8")
+    if (facebook.startsWith("\"")) {
+      facebook = facebook.substring(1)
+    }
+    if (facebook.endsWith("\"")) {
+      facebook = facebook.substring(1, facebook.length)
+    }
+    if (facebook.lastIndexOf("/https://") > -1) {
+      facebook2 = facebook.substring(facebook.lastIndexOf("/https://") + 1)
+      facebook = facebook.substring(0, facebook.lastIndexOf("/https://"))
+    }
+    facebook2 = facebook2.replace("?fref=ts", "")
+    facebook = facebook.replace("?fref=ts", "")
+    facebook2 = facebook2.replace("&fref=ts", "")
+    facebook = facebook.replace("&fref=ts", "")
+
+    var lineid2 = ""
+    var lineid = x.lineid
+    if (lineid.lastIndexOf("/") > -1) {
+      lineid2 = lineid.substring(lineid.lastIndexOf("/") + 1)
+      lineid = lineid.substring(0, lineid.lastIndexOf("/"))
+    }
+
     val wiki = URLDecoder.decode(x.wiki, "utf8")
-    Liwei(x.term, x.name, x.ename, x.sex, x.party, x.partygroup, x.areaname, x.district, email, x.committee, x.onboarddate, x.degree, x.profession, x.experience, x.alltel, x.labtel, x.servicetel1, x.servicetel2, x.servicetel3, x.servicetel4, x.servicetel5, x.labfax, x.servicefax1, x.servicefax2, x.servicefax3, x.servicefax4, x.servicefax5, x.picurl, x.leavedate, x.alladdr, x.labaddr, x.serviceaddr1, x.serviceaddr2, x.serviceaddr3, x.serviceaddr4, x.serviceaddr5, facebook, wiki, x.lineid)
+    LiweiX(x.term, x.name, x.ename, x.sex, x.party, x.partygroup, x.areaname, x.district, email, x.committee, x.onboarddate, x.degree, x.profession, x.experience, x.alltel, x.labtel, x.servicetel1, x.servicetel2, x.servicetel3, x.servicetel4, x.servicetel5, x.labfax, x.servicefax1, x.servicefax2, x.servicefax3, x.servicefax4, x.servicefax5, x.picurl, x.leavedate, x.alladdr, x.labaddr, x.serviceaddr1, x.serviceaddr2, x.serviceaddr3, x.serviceaddr4, x.serviceaddr5, facebook, wiki, lineid, facebook2, lineid2)
   }
 
   def index = Action {
@@ -81,6 +105,8 @@ class Application @Inject()(db: Database, dbapi: DBApi) extends Controller {
 }
 
 case class Liwei(term: String, name: String, ename: String, sex: String, party: String, partygroup: String, areaname: String, district: String, email: String, committee: String, onboarddate: String, degree: String, profession: String, experience: String, alltel: String, labtel: String, servicetel1: String, servicetel2: String, servicetel3: String, servicetel4: String, servicetel5: String, labfax: String, servicefax1: String, servicefax2: String, servicefax3: String, servicefax4: String, servicefax5: String, picurl: String, leavedate: String, alladdr: String, labaddr: String, serviceaddr1: String, serviceaddr2: String, serviceaddr3: String, serviceaddr4: String, serviceaddr5: String, facebook: String, wiki: String, lineid: String)
+
+case class LiweiX(term: String, name: String, ename: String, sex: String, party: String, partygroup: String, areaname: String, district: String, email: String, committee: String, onboarddate: String, degree: String, profession: String, experience: String, alltel: String, labtel: String, servicetel1: String, servicetel2: String, servicetel3: String, servicetel4: String, servicetel5: String, labfax: String, servicefax1: String, servicefax2: String, servicefax3: String, servicefax4: String, servicefax5: String, picurl: String, leavedate: String, alladdr: String, labaddr: String, serviceaddr1: String, serviceaddr2: String, serviceaddr3: String, serviceaddr4: String, serviceaddr5: String, facebook: String, wiki: String, lineid: String, facebook2: String, lineid2: String)
 
 case class Category(name: String, quantity: Int = 0)
 
