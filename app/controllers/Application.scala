c4ccf3b3fbba8221256d6cd13f674fa9b0693cac
zhengda <leonlin0403@gmail.com>
2016-10-15 22:49:18 +0800

    minor adjustments

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -23,13 +23,13 @@ class Application @Inject()(db: Database, dbapi: DBApi) extends Controller {
     Ok(views.html.about())
   }
 
-  def category(q: Option[String]) = Action {
+  def category() = Action {
     //TODO
     val x = List(Category("test"), Category("a1"), Category("c3"))
     Ok(views.html.category(x))
   }
 
-  def list(q: Option[String]) = Action {
+  def list(q: Option[String], category: Option[String]) = Action {
     import anorm.{Macro, RowParser}
     db.withConnection { implicit conn =>
       val parser: RowParser[Liwei] = Macro.indexedParser[Liwei]
@@ -37,12 +37,11 @@ class Application @Inject()(db: Database, dbapi: DBApi) extends Controller {
         case Some(value) =>
           val result: List[Liwei] = SQL("select * from t9a where name like {name} or ename like {ename}")
             .on("name" -> ("%" + value + "%"), "ename" -> ("%" + value + "%")).as(parser.*)
-          //result.foreach(println)
-          Ok(views.html.list(result))
+          Ok(views.html.list(result.map { x => repack(x) }))
         case None =>
           val result: List[Liwei] = SQL"select * from t9a ".as(parser.*)
           //result.foreach(println)
-          Ok(views.html.list(result))
+          Ok(views.html.list(result.map { x => repack(x) }))
       }
     }
   }
@@ -51,12 +50,24 @@ class Application @Inject()(db: Database, dbapi: DBApi) extends Controller {
     import anorm.{Macro, RowParser}
     db.withConnection { implicit conn =>
       val parser: RowParser[Liwei] = Macro.indexedParser[Liwei]
-      val result: Liwei = SQL(
+      val x: Liwei = SQL(
         """
            select * from t9a where name={name}
         """).on("name" -> name).as(parser.single)
-      Ok(views.html.profile(result))
+      Ok(views.html.profile(repack(x)))
     }
+  }
+
+  def repack(x: Liwei): Liwei = {
+    import java.net.URLDecoder
+    val email = if (x.email contains "不提供") {
+      ""
+    } else {
+      x.email
+    }
+    val facebook = (x.facebook + "\n").replace("?fref=ts\n", "")
+    val wiki = URLDecoder.decode(x.wiki, "utf8")
+    Liwei(x.term, x.name, x.ename, x.sex, x.party, x.partygroup, x.areaname, x.district, email, x.committee, x.onboarddate, x.degree, x.profession, x.experience, x.alltel, x.labtel, x.servicetel1, x.servicetel2, x.servicetel3, x.servicetel4, x.servicetel5, x.labfax, x.servicefax1, x.servicefax2, x.servicefax3, x.servicefax4, x.servicefax5, x.picurl, x.leavedate, x.alladdr, x.labaddr, x.serviceaddr1, x.serviceaddr2, x.serviceaddr3, x.serviceaddr4, x.serviceaddr5, facebook, wiki, x.lineid)
   }
 
   def index = Action {
@@ -64,7 +75,6 @@ class Application @Inject()(db: Database, dbapi: DBApi) extends Controller {
     db.withConnection { implicit conn =>
       val parser: RowParser[Liwei] = Macro.indexedParser[Liwei]
       val result: List[Liwei] = SQL"select * from t9a order by rand() limit 4".as(parser.*)
-      //result.foreach(println)
       Ok(views.html.index(result))
     }
   }
