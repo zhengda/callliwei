629d3c404fd3889466364264dce546378852eabe
zhengda <leonlin0403@gmail.com>
2016-10-15 13:33:15 +0800

    cleanup

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -41,7 +41,6 @@ class Application @Inject()(db: Database, dbapi: DBApi) extends Controller {
     }
   }
 
-
   def profile(name: String) = Action {
     import anorm.{Macro, RowParser}
     db.withConnection { implicit conn =>
@@ -52,88 +51,9 @@ class Application @Inject()(db: Database, dbapi: DBApi) extends Controller {
         """).on("name" -> name).as(parser.single)
       Ok(views.html.profile(result))
     }
-
-    //    var name = "";
-    //    var ename = "";
-    //    var picUrl = "";
-    //    db.withConnection { implicit conn =>
-    //      val st = conn.createStatement()
-    //      val res = st.executeQuery("select * from t9a where degree like '%法律系%'")
-    //      var i: Int = 0
-    //      while (res.next()) {
-    //        i += 1
-    //        print(i)
-    //        print(" ")
-    //        print(res.getString("term"))
-    //        print(" ")
-    //        print(res.getString("name"))
-    //        print(" ")
-    //        print(res.getString("ename"))
-    //        print(" ")
-    //        println(res.getString("LineID"))
-    //        if (i == 1) {
-    //          name = res.getString("name")
-    //          ename = res.getString("ename")
-    //          picUrl = res.getString("picurl")
-    //        }
-    //      }
-    //    }
-    //    Ok(views.html.profile(name, ename, picUrl))
   }
 
-
   def index = Action {
-    //    val db = dbapi.database("default")
-//    db.withConnection { implicit c =>
-//      val result: Boolean = SQL("Select 1").execute()
-//      println(result)
-//    }
-//    db.withConnection { implicit c =>
-//      val result = SQL("Select * from User").execute()
-//      println(result)
-//    }
-//    var name = "";
-//    var picUrl = "";
-//    db.withConnection { implicit conn =>
-//      val st = conn.createStatement()
-//      val res = st.executeQuery("SELECT * FROM t9a")
-//      var i: Int = 0
-//      while (res.next()) {
-//        i += 1
-//        print(i)
-//        print(" ")
-//        print(res.getString("term"))
-//        print(" ")
-//        print(res.getString("name"))
-//        print(" ")
-//        print(res.getString("ename"))
-//        print(" ")
-//        println(res.getString("LineID"))
-//        if (i == 1) {
-//          name = res.getString("name")
-//          picUrl = res.getString("picurl")
-//        }
-//      }
-//    }
-
-//    db.withConnection { conn =>
-//      val st = conn.createStatement()
-//      val res = st.executeQuery("SELECT * FROM PLAY_EVOLUTIONS")
-//      while (res.next()) {
-//        print(res.getInt("id"))
-//        print(" ")
-//        print(res.getString("hash"))
-//        print(" ")
-//        println(res.getString("state"))
-//      }
-//    }
-
-    //    import java.util.Date
-    //    import java.text.SimpleDateFormat
-    //    val date = new Date()
-    //    val dateStr = new SimpleDateFormat().format(date)
-    //    Ok(views.html.index(dateStr, name, picUrl))
-
     import anorm.{Macro, RowParser}
     db.withConnection { implicit conn =>
       val parser: RowParser[Liwei] = Macro.indexedParser[Liwei]
@@ -141,11 +61,7 @@ class Application @Inject()(db: Database, dbapi: DBApi) extends Controller {
       //result.foreach(println)
       Ok(views.html.index(result))
     }
-
-
   }
-
 }
-
 
 case class Liwei(term: String, name: String, ename: String, sex: String, party: String, partygroup: String, areaname: String, district: String, email: String, committee: String, onboarddate: String, degree: String, profession: String, experience: String, alltel: String, labtel: String, servicetel1: String, servicetel2: String, servicetel3: String, servicetel4: String, servicetel5: String, labfax: String, servicefax1: String, servicefax2: String, servicefax3: String, servicefax4: String, servicefax5: String, picurl: String, leavedate: String, alladdr: String, labaddr: String, serviceaddr1: String, serviceaddr2: String, serviceaddr3: String, serviceaddr4: String, serviceaddr5: String, facebook: String, wiki: String, lineid: String)
